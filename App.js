7197c6f0c300aa8861547d19a4149ed6d74e363a
xmflsct <me@xmflsct.com>
2023-02-25 14:36:42 +0100

    Commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image'
+import { Text, View } from 'react-native'
+import { Blurhash } from 'react-native-blurhash'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 32
+      }}
+    >
+      <Text>Demo expo-image is broken on some blurhashes</Text>
+      <View style={{ flexDirection: 'row', gap: 8 }}>
+        <Image
+          source='UMJ7~,%L~pxt%hahM}a#?ZkCD%WBS5Rjn~WD'
+          style={{ width: 100, height: 100, backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+        />
+        <Blurhash
+          blurhash='UMJ7~,%L~pxt%hahM}a#?ZkCD%WBS5Rjn~WD'
+          style={{ width: 100, height: 100, backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+        />
+      </View>
+      <View style={{ flexDirection: 'row', gap: 8 }}>
+        <Image
+          source='ULGuq0M{D*off-IVbbxa01WBWVt7jW%LV@Rk'
+          style={{ width: 100, height: 100, backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+        />
+        <Blurhash
+          blurhash='ULGuq0M{D*off-IVbbxa01WBWVt7jW%LV@Rk'
+          style={{ width: 100, height: 100, backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+        />
+      </View>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,3 @@
+Showcase `expo-image` is broken on some blurhashes, compared with `react-native-blurhash` side by side.
+
+This is observed only after using together with `expo` version after `48.0.0-beta.2`, basically all the GA versions would fail some blurhashes.
